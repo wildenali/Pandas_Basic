c5d721b83c2906922f7fe66763db13b1cc8687ee
rc-wilden <rc-wilden@dewatic.com>
2022-01-05 17:33:54 +0700

    index per kolom, per dua kolom, dot notation, perlocation

--- 004-Series/main_01.py
+++ 004-Series/main_01.py
@@ -31,3 +31,57 @@ print("\ndataku2")
 print(dataku2)
 print("\nPenjumlahan Series")
 print(dataku + dataku2)
+
+
+'''
+-------------numpy-------------
+[10 20 30]
+10
+
+-------------Series-------------
+satu    10
+dua     20
+tiga    30
+dtype: int64
+10
+satu    10
+dua     20
+tiga    30
+dtype: int64
+0    10
+1    20
+2    30
+dtype: int32
+0    10
+1    20
+2    30
+dtype: int64
+
+dataku_frame
+       0
+satu  10
+dua   20
+tiga  30
+
+dataku
+satu    10
+dua     20
+tiga    30
+dtype: int64
+
+dataku2
+satu     10
+dua      20
+tiga     30
+empat    40
+lima     50
+dtype: int64
+
+Penjumlahan Series
+dua      40.0
+empat     NaN
+lima      NaN
+satu     20.0
+tiga     60.0
+dtype: float64
+'''
